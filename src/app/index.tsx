26a38004633d644b06505827dc644e6e0fa702fd
Gustavo Ferreira <gustavo.soares.ferreira@hotmail.com>
2023-02-27 09:07:59 -0300

    adicionando mensagem de loading enquanto a imagem esta sendo carregada

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -4,10 +4,13 @@ import './App.css';
 
 function App() {
   const [doguinho, setDoguinho] = useState("");
+  const [loading, setLoading] = useState(true);
 
   async function getNewDog (){
+    setLoading(true)
     const dog = await getDogs();
     setDoguinho(dog)
+    setLoading(false)
   }
 
   useEffect(() => {
@@ -23,9 +26,14 @@ function App() {
       <div className='header'>
         <h1>🐶 Doguinhos 😭</h1>
       </div>
+      
       <img className='dog' src={doguinho} ></img>
     
-      <button className='button-search' onClick={async () => {await getNewDog();}} > 🔍 Buscar outro doguinho</button>
+      <button className='button-search' onClick={async () => {await getNewDog();}} >
+        {loading 
+          ? <span>Carregando</span> 
+          :<span>🔍 Buscar outro doguinho</span>}
+      </button>
     </div>
   );
 }
